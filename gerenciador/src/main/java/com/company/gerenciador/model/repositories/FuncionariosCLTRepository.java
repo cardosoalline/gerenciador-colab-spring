d1890df256f38edc19e0071db66941317d72d155
alline.cardoso <migowskialline@gmail.com>
2022-02-01 12:34:44 -0300

    funcionariorepository initial

---
+++ gerenciador/src/main/java/com/company/gerenciador/model/repositories/FuncionariosCLTRepository.java
@@ -0,0 +1,11 @@
+package com.company.gerenciador.model.repositories;
+
+import com.company.gerenciador.model.entity.FuncionarioCLT;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository //extende as interfaces que tem no springData
+public interface FuncionariosCLTRepository extends JpaRepository<FuncionarioCLT, Long> { //<T, ID>tipo da chave primária e a entidade
+
+
+}
