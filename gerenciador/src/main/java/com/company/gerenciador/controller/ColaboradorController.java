97fad78e6884c835acf03c6e6bb1b3f9a8c80190
alline.cardoso <migowskialline@gmail.com>
2022-02-01 12:32:47 -0300

    instancia do repository e metodo findall

--- gerenciador/src/main/java/com/company/gerenciador/controller/ColaboradorController.java
+++ gerenciador/src/main/java/com/company/gerenciador/controller/ColaboradorController.java
@@ -1,5 +1,8 @@
 package com.company.gerenciador.controller;
 
+import com.company.gerenciador.model.entity.FuncionarioCLT;
+import com.company.gerenciador.model.repositories.FuncionariosCLTRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +11,19 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/colaboradores")
+@AllArgsConstructor //criar construtores
 public class ColaboradorController {
+
+    //acessar o repositório //final garante que nenhuma alteração afetará a instância do repositório
+    private final FuncionariosCLTRepository funcionariosCLTRepository;
+
+
 
     //método listar todos
     @GetMapping
-    public List<Object> list(){
-        return null;
+    public List<FuncionarioCLT> list(){
+        return funcionariosCLTRepository.findAll();
     }
+
+
 }
