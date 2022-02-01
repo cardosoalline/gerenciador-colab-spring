70c5e8f2b704540e4bc2b8059ab277e82c385e3f
alline.cardoso <migowskialline@gmail.com>
2022-02-01 12:36:38 -0300

    criação de bean para teste findall

--- gerenciador/src/main/java/com/company/gerenciador/GerenciadorApplication.java
+++ gerenciador/src/main/java/com/company/gerenciador/GerenciadorApplication.java
@@ -1,7 +1,11 @@
 package com.company.gerenciador;
 
+import com.company.gerenciador.model.entity.FuncionarioCLT;
+import com.company.gerenciador.model.repositories.FuncionariosCLTRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GerenciadorApplication {
@@ -10,4 +14,17 @@ public class GerenciadorApplication {
 		SpringApplication.run(GerenciadorApplication.class, args);
 	}
 
-}
+	@Bean
+	CommandLineRunner initDatabase(FuncionariosCLTRepository funcionariosCLTRepository) {
+		return args -> {
+			funcionariosCLTRepository.deleteAll();
+
+
+			FuncionarioCLT f = new FuncionarioCLT();
+			f.setNome("teste");
+			f.setDocumento("1212");
+			f.setValorSalario(120.0);
+			funcionariosCLTRepository.save(f);
+		};
+	}
+	}
