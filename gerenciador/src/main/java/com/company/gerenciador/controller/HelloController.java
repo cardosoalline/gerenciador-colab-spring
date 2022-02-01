81dc12802d4c54c73b3b47f7705be91f912bafbe
alline.cardoso <migowskialline@gmail.com>
2022-02-01 08:53:16 -0300

    criação controller e metodo list

---
+++ gerenciador/src/main/java/com/company/gerenciador/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.company.gerenciador.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //(substitui @Controller + @ResponseBody)
+@RequestMapping("/") //direciona como a api será acessada
+public class HelloController {
+
+    @GetMapping //
+    public String hello(){
+        return "Hello alterado";
+    }
+
+}
