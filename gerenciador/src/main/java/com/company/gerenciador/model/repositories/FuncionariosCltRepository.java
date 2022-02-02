3a11794bcc673af174a327eb7d484821e4d17aa0
alline.cardoso <migowskialline@gmail.com>
2022-02-02 18:30:12 -0300

    refactor devido aos renames

--- gerenciador/src/main/java/com/company/gerenciador/model/repositories/FuncionariosCltRepository.java
+++ gerenciador/src/main/java/com/company/gerenciador/model/repositories/FuncionariosCltRepository.java
@@ -1,11 +1,12 @@
 package com.company.gerenciador.model.repositories;
 
-import com.company.gerenciador.model.entity.FuncionarioCLT;
+
+import com.company.gerenciador.model.entity.FuncionariosClt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository //extende as interfaces que tem no springData
-public interface FuncionariosCLTRepository extends JpaRepository<FuncionarioCLT, Long> { //<T, ID>tipo da chave primária e a entidade
+public interface FuncionariosCltRepository extends JpaRepository<FuncionariosClt, Long> { //<T, ID>tipo da chave primária e a entidade
 
 
 }
