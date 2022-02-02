6c00b28889fc9f7eae38369f768a193cdc4d0c8c
alline.cardoso <migowskialline@gmail.com>
2022-02-02 18:19:12 -0300

    comentario dos anotates json

--- gerenciador/src/main/java/com/company/gerenciador/model/entity/FuncionariosClt.java
+++ gerenciador/src/main/java/com/company/gerenciador/model/entity/FuncionariosClt.java
@@ -11,12 +11,12 @@ import javax.persistence.*;
 //@Table(name = "funcionariosCLT") //cria um nome diferente no bd caso queira ser != da nome da classe
 @AllArgsConstructor //cria as diversas formas de construtor
 @NoArgsConstructor //cria construtor vazio
-public class FuncionarioCLT {
+public class FuncionariosClt {
 
     @Id //informação para o bd que é uma PK
     @GeneratedValue(strategy = GenerationType.AUTO) //para gerar o ID autoincrement
    // @JsonProperty("_id") //nomeia o atributo como vai aparecer no json (faz o papel do dto)
-   // @JsonIgnore //oculta o atributo na view (faz o papel do dto)
+   //gerenciador/src/main/java/com/company/gerenciador/model/entity/FuncionariosClt.java @JsonIgnore //oculta o atributo na view (faz o papel do dto)
     private Long id;
 
     @Column (length = 255,nullable = false ) //identifica como uma coluna e pode identificar um nome diferente do declarado na classe ex:name = "name", pode colocar o tamanho do atributo, criar como not null
