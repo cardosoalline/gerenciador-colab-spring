de5a1f82069f120f77b827b05c6f8759656c6b19
alline.cardoso <migowskialline@gmail.com>
2022-02-02 18:14:15 -0300

    refactor application.java retornando ao padrao de projeto, delete hellocontroler e rename entity e repository

--- gerenciador/src/main/java/com/company/gerenciador/GerenciadorApplication.java
+++ gerenciador/src/main/java/com/company/gerenciador/GerenciadorApplication.java
@@ -1,11 +1,7 @@
 package com.company.gerenciador;
 
-import com.company.gerenciador.model.entity.FuncionarioCLT;
-import com.company.gerenciador.model.repositories.FuncionariosCLTRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GerenciadorApplication {
@@ -14,17 +10,5 @@ public class GerenciadorApplication {
 		SpringApplication.run(GerenciadorApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner initDatabase(FuncionariosCLTRepository funcionariosCLTRepository) {
-		return args -> {
-			funcionariosCLTRepository.deleteAll();
 
-
-			FuncionarioCLT f = new FuncionarioCLT();
-			f.setNome("teste");
-			f.setDocumento("1212");
-			f.setValorSalario(120.0);
-			funcionariosCLTRepository.save(f);
-		};
-	}
 	}
--- gerenciador/src/main/java/com/company/gerenciador/model/entity/FuncionariosClt.java
+++ gerenciador/src/main/java/com/company/gerenciador/model/entity/FuncionariosClt.java
@@ -1,18 +1,22 @@
 package com.company.gerenciador.model.entity;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Data //gera getter, setter, construtor, tosrting, equals e hashcode
 @Entity //especifica essa classe como uma entidade que fará mapeando com o bd
 //@Table(name = "funcionariosCLT") //cria um nome diferente no bd caso queira ser != da nome da classe
+@AllArgsConstructor //cria as diversas formas de construtor
+@NoArgsConstructor //cria construtor vazio
 public class FuncionarioCLT {
 
     @Id //informação para o bd que é uma PK
     @GeneratedValue(strategy = GenerationType.AUTO) //para gerar o ID autoincrement
+   // @JsonProperty("_id") //nomeia o atributo como vai aparecer no json (faz o papel do dto)
+   // @JsonIgnore //oculta o atributo na view (faz o papel do dto)
     private Long id;
 
     @Column (length = 255,nullable = false ) //identifica como uma coluna e pode identificar um nome diferente do declarado na classe ex:name = "name", pode colocar o tamanho do atributo, criar como not null
